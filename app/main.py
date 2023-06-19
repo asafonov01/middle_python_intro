9ff23a25e599f2f14d1a15e762c9d6e94f55de31
AleksSafonov <aleks.safonov01@gmail.com>
2023-06-20 00:01:37 +0300

    Фикс линтера

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
+"""Генератор приветствий."""
 import sys
 
 
 def greeting(name: str) -> str:
+    """Возвращает текст приветствия.
 
+    Args:
+        name(str): Имя пользователя
+
+    Returns:
+        str: Текст приветствия
+    """
     return 'Привет, {0}'.format(name.title())
 
 
