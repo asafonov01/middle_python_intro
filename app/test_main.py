145077acf247391c5b891f84c7f5cee7c04414b0
AleksSafonov <aleks.safonov01@gmail.com>
2023-06-19 23:39:15 +0300

    Исправлены стилистические ошибки

--- app/test_main.py
+++ app/test_main.py
@@ -5,7 +5,7 @@ from main import greeting
 @pytest.mark.parametrize(
     'name,expected',
     [('Никита', 'Привет, Никита'), ('Ольга', 'Привет, Ольга')],
-)
+    )
 def test_greeting(name: str, expected: str):
     assert greeting(name) == expected
 
